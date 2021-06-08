5b8de4d26392956ebe921f405dc480cab7a98367
Farelanders <marcostravagin@outlook.com>
2021-06-08 16:49:13 -0300

    adição de token de autenticação e session controller e put

--- src/router.js
+++ src/router.js
@@ -2,9 +2,14 @@ import { Router } from 'express';
 const routes = new Router();
 import Database from './database/index'
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController'
+import authMiddleware from './app/middlewares/auth'
 
 routes.post('/users', UserController.store)
+routes.post('/session', SessionController.store)
 
-
+// Rotas autenticadas
+routes.use(authMiddleware)
+routes.put('/users', UserController.update)
  
 export default routes;
